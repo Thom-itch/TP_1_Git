fae832a0efb6bde258f62087e2bc84831156ad5e
thoma <toto.francill@gmail.com>
2025-01-27 18:05:37 +0100

    Documentation de la classe DirectionCourse (sans la génération de la javadoc)

--- Route_Rhum_Observateur/src/DirectionCourse.java
+++ Route_Rhum_Observateur/src/DirectionCourse.java
@@ -10,36 +10,49 @@ public class DirectionCourse implements Observable { // Implémente l'interface 
     List<Observateur> listObservateurs; // Liste des observateurs enregistrés
     List<Abandon> listAbandons; // Liste des abandons enregistrés
 
-
-    // Constructeur : initialise les listes d'observateurs et d'abandons
+    /**
+     * Inialise les listes d'observateurs et d'abandons
+     **/
     public DirectionCourse() {
         listObservateurs = new LinkedList<>(); // Liste pour stocker les observateurs
         listAbandons = new LinkedList<>(); // Liste pour stocker les abandons
     }
 
-    // Méthode pour enregistrer un observateur
+    /**
+     * Enregistre un observateur
+     **/
     public void enregistreObservateur(Observateur o) {
         listObservateurs.add(o); // Ajoute l'observateur à la liste
     }
 
+    /**
+     * Desenregistre un observateur
+     **/
     @Override
     public void desenregistreObservateur(Observateur o) { // Retire un observateur de la liste
         listObservateurs.remove(o); // Supprime l'observateur donné
     }
 
-    // Notifie tous les observateurs enregistrés des changements
+    /**
+     * Notifie tous les observateurs enregistres des changements
+     **/
     public void informeObservateurs() {
         for (Observateur o : listObservateurs) { // Parcourt la liste des observateurs
             o.actualise(this); // Appelle la méthode actualise pour chaque observateur
         }
     }
 
-    // Renvoie le dernier abandon enregistré dans la liste
+    /**
+     * Renvoie le dernier abandon enregistre dans la liste
+     * @return listAbandons
+     **/
     public Abandon getDernierAbandon() {
         return listAbandons.get(listAbandons.size() - 1); // Retourne le dernier élément de la liste
     }
 
-    // Simule une série d'abandons en les ajoutant à la liste et en notifiant les observateurs
+    /**
+     * Renvoie le dernier abandon enregistré dans la liste
+     **/
     public void simulation(HashSet<Abandon> lesAbandons) {
         for (Iterator<Abandon> i = lesAbandons.iterator(); i.hasNext();) { // Parcourt l'ensemble des abandons
             Abandon abandon = i.next(); // Récupère l'abandon courant
