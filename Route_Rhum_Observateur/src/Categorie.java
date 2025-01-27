c244899896c3d3a429f3aab44cc9f91bb1922608
thoma <toto.francill@gmail.com>
2025-01-27 17:40:22 +0100

    Documentation de la classe Categorie (sans la génération de la javadoc)

--- Route_Rhum_Observateur/src/Categorie.java
+++ Route_Rhum_Observateur/src/Categorie.java
@@ -8,15 +8,21 @@ public class Categorie implements Observateur { // Implémente l'interface Obser
     ClasseVoilier classeVoilier; // Définit la classe de voilier associée à cette catégorie
     List<Abandon> listAbandons; // Liste pour stocker les abandons correspondant à cette catégorie
 
-
-    // Constructeur pour initialiser une catégorie avec une classe spécifique de voilier
+    /**
+     * Initialise une categorie avec une classe spécifique de voilier
+     * @param classeVoilier
+     **/
     public Categorie(ClasseVoilier classeVoilier) {
         this.classeVoilier = classeVoilier; // Initialise l'attribut classeVoilier
         listAbandons = new ArrayList<>(); // Crée une liste vide pour les abandons
     }
 
+    /**
+     * Notifie un changement de l'objet observe (actualise l'objet observe)
+     * @param o
+     **/
     @Override
-    public void actualise(Observable o) { // Méthode appelée pour notifier un changement de l'objet observé
+    public void actualise(Observable o) {
         DirectionCourse directionCourse = (DirectionCourse) o; // Cast l'objet Observable en DirectionCourse
         Abandon abandon = directionCourse.getDernierAbandon(); // Récupère le dernier abandon signalé
         System.out.println("\n" + "actualise" + " " + abandon.getClasseVoilier() + this.classeVoilier);
@@ -27,7 +33,9 @@ public class Categorie implements Observateur { // Implémente l'interface Obser
         }
     }
 
-    // Méthode toString pour représenter la catégorie sous forme de chaîne de caractères
+    /**
+     * Represete la categorie sous forme de chaine de caractere
+     **/
     public String toString() {
         return "\n" + classeVoilier.toString() + " " + listAbandons.size() + "  " + listAbandons;
         // Fournit une représentation de la classe, du nombre d'abandons, et de leur liste
